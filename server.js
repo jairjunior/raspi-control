dc133b9a44aa51cdb4cbcda881305becfafe5e9a
Jair Junior <j.junior89@hotmail.com>
2020-11-03 13:55:41 -0300

    First commit with node.js server and htmlindex page already running.

---
+++ server.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const fs = require('fs');
+
+
+const server = http.createServer( (req, res) => {
+	if(req.url === '/'){
+		fs.readFile('./_views/index.html', 'utf8', (err, data) => {
+			if(err) throw err;
+			res.writeHead(200, {'Content-Type': 'text/html'});
+			res.write(data);
+			res.end();
+		});
+	}
+	else if(req.url === '/leds'){
+		res.write('LEDs page!');
+		res.end();
+	}
+	else if (req.url === '/buttons'){
+		res.write('Buttons page!');
+		res.end();
+	}
+	else if (req.url === '/reqbody'){
+		console.log('Method: ', req.method);
+		console.log('URL: ', req.url);
+		console.log('HTTP headers: ', req.headers);
+		console.log('Request body: ', req.body);
+		console.log('--------------------------------');
+		res.end();
+	}
+});
+	
+server.listen(3000, () => {
+	console.log('Server listening on port 3000...');
+});
