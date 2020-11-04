b19eed9f9803ec5d07d273322adefd618ada8a4e
Jair Junior <j.junior89@hotmail.com>
2020-11-04 18:48:30 -0300

    Now each link of the sidebar menu has some animation. Scrolling while the sidebar menu is open is avoided now.

--- _js/top-navbar.js
+++ _js/top-navbar.js
@@ -3,11 +3,23 @@
 function slideNavBar(){
      const burgerButton = document.querySelector('.burger-button');
      const sideBarMenu = document.querySelector('.top-navlinks');
-     const navLinks = document.querySelectorAll('#top-navlinks > li');
+     const navLinks = document.querySelectorAll('.top-navlinks > li');
+     const bodyTag = document.querySelector('body');
+     console.log('bodyTag ', bodyTag);
 
      burgerButton.addEventListener('click', () => {
+          // Avoid scrolling the page while sidebar is open
+          bodyTag.classList.toggle('overflow-hidden');
+
+          // Toggle sidebar Menu
           sideBarMenu.classList.toggle('sidebarmenu-show');
-          console.log('Click on burger!');
+
+          // Sidebar links
+          navLinks.forEach( (link, i) => {
+               if(link.style.animation)
+                    link.style.animation = '';
+               else link.style.animation = 'topNavLinksSlide 1.3s ease-in-out forwards';
+          });
      });
 }
 
